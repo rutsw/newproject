c1eb6ec64de04a7b84965a0ad24e00f3d89195f7
DESKTOP-VJCI8VR\mariella <miryam993@gmail.com>
2017-02-08 12:18:09 +0200

    request

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,12 +1,42 @@
 module.exports = {
-
-  // show the home page
-  // showHome: (req, res) => {
-  //   res.render('pages/home');
-  // }
-
-showContact: (req, res) => {
-    res.render('pages/contact');
-  }
-
+	showContact: showContact,
+    sendRequest: sendRequest
 };
+
+
+function showContact(req, res) {
+    res.render('pages/contact');
+ }
+
+/**
+ * Send request
+ */
+function sendRequest(req, res) {
+  //send mail
+  var transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    auth: {
+        user: 'miryam993@gmail.com',
+        pass: 'mary010293'
+    }
+  }));
+  
+  console.log('name is:' + req.body.name);
+  var mailOptions = {
+    from: req.body.email, // sender address
+    to: 'tamimamo@gmail.com', // list of receivers
+    subject: 'מייל מהאתר הדרך להציל חיים', // Subject line
+    text: " נשלח מאת: "+req.body.name+"\n כתובת מייל: "+req.body.email+"\n טלפון: "+req.body.phone+"\n תוכן ההודעה: "+req.body.message // plaintext body
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+          res.json({yo: 'error'});
+      }else{
+          console.log('Message sent: ' + info.response);
+          //res.json({yo: info.response});
+      };
+      res.redirect(`/contact`);
+    });
+}
+
