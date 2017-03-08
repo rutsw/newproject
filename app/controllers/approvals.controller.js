eec1d9ed0c599e33b127b839fbf23bb6fc72bb65
tamimamo <tamimamo@gmail.com>
2017-03-08 22:10:20 +0200

    controllers structure changes 3

--- app/controllers/approvals.controller.js
+++ app/controllers/approvals.controller.js
@@ -1,8 +1,26 @@
 module.exports = {
+  showApprovals: showApprovals
+}
 
-  // show the approvals page
-	showApprovals: (req, res) => {
-    res.render('pages/approvals');
-  }
 
-};
+// show the approvals page
+function showApprovals(req, res){
+    
+    var userstat_si_so="<a class=\"index\" id=\"signin\" href=\"/login\">התחבר</a>";
+    var userstat_su_un="<a class=\"index\" id=\"signup\" href=\"/register\">/הרשם</a>";
+    
+    //check if the user is conected
+    if (req.isAuthenticated()){
+        userstat_su_un = " שלום, "+req.user.local.username;
+        userstat_si_so = "<a class=\"index\" id=\"signout\" href=\"/logout\">/התנתק</a>";
+        
+        res.render('pages/approvals', {userstat_su_un:userstat_su_un ,userstat_si_so:userstat_si_so});
+    }
+      
+    // return a view with data in case user didn't connect
+    else{
+        
+        res.render('pages/approvals', { userstat_su_un: userstat_su_un ,userstat_si_so:userstat_si_so  });
+    }
+    
+}
