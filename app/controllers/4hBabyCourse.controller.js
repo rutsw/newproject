e102c55f0f669df3467c23a2f641a442f176d440
tamimamo <tamimamo@gmail.com>
2017-03-08 22:00:39 +0200

    controllers structure changes 2

--- app/controllers/4hBabyCourse.controller.js
+++ app/controllers/4hBabyCourse.controller.js
@@ -1,13 +1,27 @@
-
-
 module.exports = {
   show4hBabyCourse: show4hBabyCourse
 }
 
 
-  // show 4h baby course page
+// show 4h baby course page
 function show4hBabyCourse(req, res){
     
-    res.render('pages/4hBabyCourse');
+    var userstat_si_so="<a class=\"index\" id=\"signin\" href=\"/login\">התחבר</a>";
+    var userstat_su_un="<a class=\"index\" id=\"signup\" href=\"/register\">/הרשם</a>";
+    
+    //check if the user is conected
+    if (req.isAuthenticated()){
+        userstat_su_un = " שלום, "+req.user.local.username;
+        userstat_si_so = "<a class=\"index\" id=\"signout\" href=\"/logout\">/התנתק</a>";
+        
+        res.render('pages/4hBabyCourse', {userstat_su_un:userstat_su_un ,userstat_si_so:userstat_si_so});
+    }
+      
+    // return a view with data in case user didn't connect
+    else{
+        
+        res.render('pages/4hBabyCourse', { userstat_su_un: userstat_su_un ,userstat_si_so:userstat_si_so  });
+    }
+    
 }
 
--- app/controllers/4hCourse.controller.js
+++ app/controllers/4hCourse.controller.js
@@ -6,6 +6,22 @@ module.exports = {
 // show 4h course page
 function show4hCourse(req, res){
     
-    res.render('pages/4hCourse');
+    var userstat_si_so="<a class=\"index\" id=\"signin\" href=\"/login\">התחבר</a>";
+    var userstat_su_un="<a class=\"index\" id=\"signup\" href=\"/register\">/הרשם</a>";
+    
+    //check if the user is conected
+    if (req.isAuthenticated()){
+        userstat_su_un = " שלום, "+req.user.local.username;
+        userstat_si_so = "<a class=\"index\" id=\"signout\" href=\"/logout\">/התנתק</a>";
+        
+        res.render('pages/4hCourse', {userstat_su_un:userstat_su_un ,userstat_si_so:userstat_si_so});
+    }
+      
+    // return a view with data in case user didn't connect
+    else{
+        
+        res.render('pages/4hCourse', { userstat_su_un: userstat_su_un ,userstat_si_so:userstat_si_so  });
+    }
+    
 }
 
