e3470b6d99170bb1fdd85fec9c26520b49dc03e1
DESKTOP-VJCI8VR\mariella <miryam993@gmail.com>
2017-03-15 21:41:55 +0200

    correct route

--- app/controllers/trainingandSimulationEquipment.controller.js
+++ app/controllers/trainingandSimulationEquipment.controller.js
@@ -15,7 +15,7 @@ function showTrainingandSimulationEquipment(req, res){
   var userstat_su_un="<a class=\"index\" id=\"signup\" href=\"/register\">הרשם/</a>";
     
   // get all products   
-  Product.find({category: "training and simulation equipment"}, (err, products) => {
+  Product.find({category: "trainingand equipment"}, (err, products) => {
     if (err) {
       res.status(404);
       res.send('Products not found!');
